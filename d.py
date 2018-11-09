8e411ead59e21a5d57b506df6dd3372979db25a9
darkcode357 <darkcode357@gmail.com>
2018-11-09 16:14:47 -0200

    init

--- d.py
+++
@@ -1,5 +0,0 @@
-for i in range(100000000000000000000):
-    lista = ['darkcode', 'dark0x00', 'xzxx', 'DarkCode', 'pwn']
-    lista.append(i)
-    for a in lista:
-       print(a)
--- dsadsa.py
+++
@@ -1,8 +0,0 @@
-import psycopg2 
-conn = psycopg2.connect(host="192.168.222.20",port=6554,database="dbsimec", user="simec", password="phpsimecalt")
-b = cur = conn.cursor()
-a = cur.execute('SELECT version()')
-db_version = cur.fetchone()
-print(b)
-print(a)
-print(db_version)
